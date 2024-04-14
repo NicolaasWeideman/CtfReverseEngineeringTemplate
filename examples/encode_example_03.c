1cbf123f2723201376a843da4fd612c2c2851fe9
nicolaas <nweidema@usc.edu>
2024-04-13 17:41:58 -0700

    Fix bug in allocating encoded guess buffer

--- examples/encode_example_03.c
+++ examples/encode_example_03.c
@@ -4,7 +4,7 @@
 void __attribute__ ((noinline)) encode(char *guess, long unsigned int guess_len, char **encoded_guess_out, long unsigned int *encoded_guess_len_out)
 {
     long unsigned int encoded_guess_len = guess_len;
-    char *encoded_guess = malloc(guess_len * sizeof (char));
+    char *encoded_guess = malloc(encoded_guess_len * sizeof (char));
 
     long unsigned int i;
     for (i = 0; i < guess_len; i++) {
